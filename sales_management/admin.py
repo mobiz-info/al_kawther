166eff5dbcfe20b9f3ffd40c217274dcfe8325b7
Josnacc <josnacc96@gmail.com>
2025-12-06 12:17:00 +0530

    transaction

--- sales_management/admin.py
+++ sales_management/admin.py
@@ -3,7 +3,14 @@ from django.contrib import admin
 from . models import *
 
 # Register your models here.
-admin.site.register(CollectionPayment)
-admin.site.register(CollectionItems)
+class CollectionPaymentAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in CollectionPayment._meta.get_fields()]
+
+# Dynamically get all fields for CollectionItems
+class CollectionItemsAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in CollectionItems._meta.get_fields()]
+
+admin.site.register(CollectionPayment, CollectionPaymentAdmin)
+admin.site.register(CollectionItems, CollectionItemsAdmin)
 admin.site.register(CollectionCheque)
 admin.site.register(Receipt)
