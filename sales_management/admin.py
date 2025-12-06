75bf048567d3564982795cf8d85313c0882c9507
Josnacc <josnacc96@gmail.com>
2025-12-06 12:18:39 +0530

    transaction

--- sales_management/admin.py
+++ sales_management/admin.py
@@ -4,13 +4,34 @@ from . models import *
 
 # Register your models here.
 class CollectionPaymentAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in CollectionPayment._meta.get_fields()]
+    # Show all fields dynamically
+    list_display = [field.name for field in CollectionPayment._meta.get_fields() if not field.many_to_many and not field.one_to_many]
 
-# Dynamically get all fields for CollectionItems
+    # Searchable fields: CharField/TextField + related fields
+    search_fields = [
+        'receipt_number',
+        'customer__name',    # Assuming Customers has 'name' field
+        'salesman__username',# Assuming CustomUser has 'username' field
+        'payment_method'
+    ]
+
+    # Optional: Add filters for ForeignKeys or choices
+    list_filter = ('payment_method', 'salesman', 'created_date')
+
+# Admin for CollectionItems
 class CollectionItemsAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in CollectionItems._meta.get_fields()]
+    list_display = [field.name for field in CollectionItems._meta.get_fields() if not field.many_to_many and not field.one_to_many]
+
+    search_fields = [
+        'invoice__invoice_number',    # Assuming Invoice has invoice_number field
+        'collection_payment__receipt_number',
+        'collection_payment__customer__name'
+    ]
+
+    list_filter = ('collection_payment__payment_method',)
 
 admin.site.register(CollectionPayment, CollectionPaymentAdmin)
 admin.site.register(CollectionItems, CollectionItemsAdmin)
+
 admin.site.register(CollectionCheque)
 admin.site.register(Receipt)
