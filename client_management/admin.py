1cf9368a91b46f4c5302afce0aa585b38923d968
Josnacc <josnacc96@gmail.com>
2025-12-10 16:26:48 +0530

    update

--- client_management/admin.py
+++ client_management/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
+from django.utils import timezone
 
 # Register your models here.
 from . models import *
